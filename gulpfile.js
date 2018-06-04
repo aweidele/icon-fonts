e1e282fd51c11e3ab25c2bfdf5affa1031505fb0
R2INTEGRATED\aweidele <aweidele@r2integrated.com>
2018-06-04 11:20:12 -0400

    create sample icons page

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,13 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
 var pump = require('pump');
 var iconfont = require('gulp-iconfont');
 var iconfontCss = require('gulp-iconfont-css');
 
-var sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'expanded'
-};
-
-var autoprefixerOptions = {
-  browsers: ['last 2 versions']
-};
-
-gulp.task('sass', function(){
-  return gulp
-    .src('src/sass/main.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass(sassOptions).on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(gulp.dest('css/'))
-});
-
 gulp.task('iconfont', function(){
-  gulp.src(['src/icons/*.svg'])
+  gulp.src(['icons/*.svg'])
     .pipe(iconfontCss({
       fontName: 'icon_font',
-      targetPath: '../src/sass/_icons.css',
+      targetPath: '../css/icons.css',
       fontPath: '../fonts/'
     }))
     .pipe(iconfont({
@@ -42,8 +20,7 @@ gulp.task('iconfont', function(){
 
 
 gulp.task('watch', function(){
-  gulp.watch('src/sass/**/*.scss', ['sass']);
-  gulp.watch('src/icons/**/*.svg', ['iconfont','sass']);
+  gulp.watch('icons/**/*.svg', ['iconfont']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['watch']);
